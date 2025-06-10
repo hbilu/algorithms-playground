55800bf58ddb3d54f6a9b27d9d1d35b84c0e29a5
hbilu <hacerbilu@gmail.com>
2025-06-10 14:11:27 +0200

    Add additional test cases for binary search

--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -9,3 +9,24 @@ def test_binary_search_found():
 
 def test_binary_search_not_found():
     assert binary_search([1, 3, 5, 7, 9], 4) == -1
+
+def test_binary_search_first():
+    assert binary_search([1, 3, 5, 7, 9], 1) == 0
+
+def test_binary_search_last():
+    assert binary_search([1, 3, 5, 7, 9], 9) == 4
+
+def test_binary_search_not_found_below():
+    assert binary_search([10, 20, 30], 5) == -1
+
+def test_binary_search_not_found_above():
+    assert binary_search([10, 20, 30], 35) == -1
+
+def test_binary_search_empty_list():
+    assert binary_search([], 42) == -1
+
+def test_binary_search_single_element_found():
+    assert binary_search([42], 42) == 0
+
+def test_binary_search_single_element_not_found():
+    assert binary_search([42], 99) == -1
